708b0708e6e5b913f793f8b09264cb357baf4a94
Victor Lucas <v.santos03@icloud.com>
2023-10-26 13:50:24 -0300

    new: script js do projeto [devwebi]

---
+++ devweb-i/assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.transition').style.opacity = '1';
+  });
+  
+//   document.querySelector('.transition').addEventListener('transitionend', function () {
+//     if (window.location.href.includes('pagina2.html')) {
+//       window.location.href = 'pagina2.html';
+//     }
+//   });
+  
